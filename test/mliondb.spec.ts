ead7bda5e55eaf8b98446180584b19b20d7fcfee
lion <cwq0312@163.com>
2021-06-11 10:38:05 +0800

    add

---
+++ test/mliondb.spec.ts
@@ -0,0 +1,25 @@
+import assert from "assert";
+import LionDB from "../src/index";
+import path from "path";
+import cluster from 'cluster';
+
+let db = LionDB.cluster({filename:  path.resolve("_local/2"), env: "cluster", isMaster: true, thread: cluster });
+console.info(">>>", path.resolve("_local"));
+
+beforeEach(async function() {
+   await wait(1000);
+   await db.del("aa");
+   await db.set("aa", { name: "li lei" });
+});
+
+describe("比较取值", function() {
+   it("比较取值是否相等", async function() {
+      const vv = await db.get("aa");
+      assert.deepStrictEqual(vv.name, "li lei");
+   });
+});
+async function wait(ttl) {
+   return new Promise((resolve) => {
+      setTimeout(() => resolve(undefined), ttl);
+   });
+}
