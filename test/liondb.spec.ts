83933568c6ffba5265b9c2b4da0284ddcbefa797
lion <cwq0312@163.com>
2021-06-11 10:28:42 +0800

    add

--- test/liondb.spec.ts
+++ test/liondb.spec.ts
@@ -1,16 +1,23 @@
 import assert from "assert";
 import LionDB from "../src/index";
 import path from "path";
-let db = new LionDB(path.resolve("/_local"));
+let db = new LionDB(path.resolve("_local"));
+console.info(">>>", path.resolve("_local"));
 
 beforeEach(async function() {
-  await db.del("aa");
-  await db.set("aa", { name: "li lei" });
+   await wait(1000);
+   await db.del("aa");
+   await db.set("aa", { name: "li lei" });
 });
 
 describe("比较取值", function() {
-  it("比较取值是否相等", async function() {
-    const vv = await db.get("aa");
-    assert.deepStrictEqual(vv.name, "li lei");
-  });
+   it("比较取值是否相等", async function() {
+      const vv = await db.get("aa");
+      assert.deepStrictEqual(vv.name, "li lei");
+   });
 });
+async function wait(ttl) {
+   return new Promise((resolve) => {
+      setTimeout(() => resolve(undefined), ttl);
+   });
+}
