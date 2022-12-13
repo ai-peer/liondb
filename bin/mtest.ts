f5e14d4552b2e5f8ccab3b1442be27095253dda1
sd <leowest1921@gmail.com>
2022-12-13 11:29:28 +0800

    add

--- bin/mtest.ts
+++ bin/mtest.ts
@@ -14,22 +14,23 @@ let db = LionDB.worker({
 async function execr() {
    console.info("execr===========", db.find);
    await db.set("ref_a", "aa2");
-   await db.set("aa2", { name: "aa2", age: 12 });
+   await db.set("ba2", { name: "ba2", age: 19 });
 
    let v0 = await db.get("aa2");
    console.info("v0", v0);
 
-   let refVal = await db.find({ key: "ref_a*", query: {}, isRef: true });
+   let refVal = await db.find({ key: "ref_a*", query: {}});
    console.info("===refVal", refVal);
 
    let list = await db.find({
-      key: "*",
+      key: "a*",
+      flow: true,
       //index: "code",
       limit: 3,
-      query: { name: "*2", $gt: { age: 10 } },
+      query: { name: "*2", $gt: { age: 13 } },
    });
    console.info("list==", list);
-
+/* 
    let list2: any[] = [];
    await db.iterator(
       {
@@ -44,9 +45,9 @@ async function execr() {
       },
    );
    console.info("list2==", list2);
-
+ */
    //////////////////////////
-   let itCount = 0,
+ /*   let itCount = 0,
       start = 0;
    while (true) {
       start = Date.now();
@@ -56,7 +57,7 @@ async function execr() {
          await wait(10);
       });
       console.info("ttl=", Math.ceil((Date.now() - start) / 1000), itCount);
-   }
+   } */
 }
 async function wait(ttl) {
    return new Promise((resolve) => {
